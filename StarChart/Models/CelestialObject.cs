1cfb4bf18d516735400faa91d053467e70bbcc33
Davorin <davorin.jurkovic@gmail.com>
2021-09-04 15:37:07 +0200

    Create CelestialObject No 2

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -14,7 +14,13 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
 
+        public int? OrbitedObjectId { get; set; }
+
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
+
+
     }
 }
